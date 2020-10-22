d5df70bc1b7287c44c0dee3317b2cd633c2cccc5
ZachariahRosenberg <zachariah.rosenberg@gmail.com>
2020-10-22 15:08:15 -0400

    added .env support. Updated Readme

--- create_sizing.py
+++ create_sizing.py
@@ -1,223 +1,232 @@
-import json, requests, time
-from luis_sdk import LUISClient
-from pygame   import mixer
+import os, json, time, requests
 import speech_recognition as sr
+from luis_sdk    import LUISClient
+from pygame      import mixer
 from gsheets_sdk import gSheet_Client
+from dotenv      import load_dotenv
 
 
 class Voice_Sizer():
 
-	def __init__(self, map_key, speech_key, luis_appid, luis_appkey, gSheet_sskey, use_mic=True):
+    def __init__(self, map_key, speech_key, luis_appid, luis_appkey, gSheet_sskey, use_mic=True):
 
-		self.map_key    = map_key
-		self.speech_key = speech_key
+        self.map_key    = map_key
+        self.speech_key = speech_key
 
-		self.luis   = LUISClient(luis_appid, luis_appkey, True)
-		self.gSheet = gSheet_Client(gSheet_sskey)
+        self.luis   = LUISClient(luis_appid, luis_appkey, True)
+        self.gSheet = gSheet_Client(gSheet_sskey)
 
-		self.use_mic = use_mic
-		self.begin()
-	
-	# Starts voice flow, prompts user and sends to LUIS API
-	def begin(self):
+        self.use_mic = use_mic
+        self.begin()
+    
+    # Starts voice flow, prompts user and sends to LUIS API
+    def begin(self):
 
-		response = self.ask_user('Hello, how can I help you?')
-		print('user request: ', response)
+        response = self.ask_user('Hello, how can I help you?')
+        print('user request: ', response)
 
-		print('sending request to LUIS...')
-		self.luis.predict(response, {'on_success': self.operator, 'on_failure': lambda err: print(err)})
+        print('sending request to LUIS...')
+        self.luis.predict(response, {'on_success': self.operator, 'on_failure': lambda err: print(err)})
 
-	# Callback for LUIS Intent response. Routes intent to controller
-	def operator(self, res):
-		
-		intent = res.get_top_intent().get_name()
+    # Callback for LUIS Intent response. Routes intent to controller
+    def operator(self, res):
 
-		if intent == 'Create Sizing':
+        intent = res.get_top_intent().get_name()
 
-			self.text_to_speech('Got it, you would like to create a sizing.')
-			print('Got it, you would like to create a sizing.')
-			self.create_sizing()
+        if intent == 'Create Sizing':
 
-		elif intent == 'Something Else':
+            self.text_to_speech('Got it, you would like to create a sizing.')
+            print('Got it, you would like to create a sizing.')
+            self.create_sizing()
 
-			self.text_to_speech('Ok, I have understood that you do not know what you would like. Typical. Here\'s a fascinating thought.')
-			print('???')
-			self.get_thought()
+        elif intent == 'Something Else':
 
-		else:
-			print('I\'m sorry, I didn\'t catch that')
-			self.begin()
+            self.text_to_speech('Ok, I have understood that you do not know what you would like. Typical. Here\'s a fascinating thought.')
+            print('???')
+            self.get_thought()
 
-	# Controller for create sizing
-	def create_sizing(self):
+        else:
+            print('I\'m sorry, I didn\'t catch that')
+            self.begin()
 
-		address = self.ask_user('what is the address of the property?')
+    # Controller for create sizing
+    def create_sizing(self):
 
-		try:
-			bing_address = self.get_bing_address(address)
-			print('bing address: ', ' '.join(bing_address.values()))
-		except:
-			self.text_to_speech('I am sorry, I didn\'t quite get that.')
-			self.create_sizing()
+        address = self.ask_user('What is the address of the property?')
 
-		income = self.ask_user('how much did the property collect in rent last year?')
-		print('income reported: ',income)
+        try:
+            bing_address = self.get_bing_address(address)
+            print('bing address: ', ' '.join(bing_address.values()))
+        except:
+            self.text_to_speech('I am sorry, I didn\'t quite get that.')
+            self.create_sizing()
 
-		expense = self.ask_user('what were the total expenses for the property last year?')
-		print('expense: ', expense)
+        income = self.ask_user('How much did the property collect in rent last year?')
+        print('income reported: ',income)
 
-		units = self.ask_user('How many units are available at the property?')
-		print('units: ', units)
+        expense = self.ask_user('What were the total expenses for the property last year?')
+        print('expense: ', expense)
 
-		self.text_to_speech('Alright, sizing property now.')
-		print('Sizing...')
+        units = self.ask_user('How many units are available at the property?')
+        print('units: ', units)
 
-		t1 = time.clock()
-		self.set_uw_values('Inputs!B5', ' '.join(bing_address.values()))
-		self.set_uw_values('Inputs!B22', income)
-		self.set_uw_values('Inputs!B51', expense)
-		self.set_uw_values('Inputs!B14', units)
-		t2 = time.clock()
-		time_elapsed = t2-t1
+        self.text_to_speech('Thank you, sizing property now.')
+        print('Sizing...')
 
-		self.text_to_speech('Finished with sizing, pulling FHA loan terms')
-		print('Pulling latest FHA terms')
+        t1 = time.clock()
+        self.set_uw_values('Inputs!B5', ' '.join(bing_address.values()))
+        self.set_uw_values('Inputs!B22', income)
+        self.set_uw_values('Inputs!B51', expense)
+        self.set_uw_values('Inputs!B14', units)
+        t2 = time.clock()
+        time_elapsed = t2-t1
 
-		fha_loan = self.get_uw_values()
+        self.text_to_speech('Finished with sizing, pulling FHA loan terms')
+        print('Pulling latest FHA terms')
 
-		if float(fha_loan['Loan Amount With Fee'][1:].replace(',', '')) > 1:
-			quote = '''
-				We have a loan amount of {} at a {} rate for a {} term. 
-				This sizing was completed in {} seconds. 
-				'''.format(fha_loan['Loan Amount With Fee'], fha_loan['Rate With Fee'], fha_loan['Loan Type'], round(time_elapsed, 2))
-			print(quote)
-			self.text_to_speech(quote+' Beat that Ryan Patterson!')
-		else:
-			quote = 'I am sorry, but this property is not a fit for the FHA program. Let\'s try with CMBS instead...'
-			print(quote)
-			self.text_to_speech(quote)
-		
-	# Prompts user for input via mic/text.
-	def ask_user(self, request):
+        fha_loan = self.get_uw_values()
 
-		self.text_to_speech(request)
-		print(request)
+        if float(fha_loan['Loan Amount With Fee'][1:].replace(',', '')) > 1:
+            quote = '''
+                We have a loan amount of {} at a {} rate for a {} term. 
+                This sizing was completed in {} seconds. 
+                '''.format(fha_loan['Loan Amount With Fee'], fha_loan['Rate With Fee'], fha_loan['Loan Type'], round(time_elapsed, 2))
+            print(quote)
+            self.text_to_speech(quote+' Beat that Ryan Patterson!')
+        else:
+            quote = 'I am sorry, but this property is not a fit for the FHA program. Let\'s try with CMBS instead...'
+            print(quote)
+            self.text_to_speech(quote)
+        
+    # Prompts user for input via mic/text.
+    def ask_user(self, request):
 
-		if self.use_mic:
-			r = sr.Recognizer()
-			with sr.Microphone() as source:
-				r.adjust_for_ambient_noise(source)
-				self.play_prompt()
-				print("Recording...")
-				audio = r.listen(source)
-				print("Got it, sending to VoiceRecog...")
+        self.text_to_speech(request)
+        print(request)
 
-			try:	
-				response = r.recognize_bing(audio, key=self.speech_key)
-			except e:
-				print("Speech Recognition failed: {0}".format(e))
-				response = input('Let\'s try typing the request instead -->\n')
+        if self.use_mic:
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source)
+                self.play_prompt()
+                print("Recording...")
+                audio = r.listen(source)
+                print("Got it, sending to VoiceRecog...")
 
-		else:
-			response = input('please type your response\n')
+            try:	
+                response = r.recognize_bing(audio, key=self.speech_key)
+            except e:
+                print("Speech Recognition failed: {0}".format(e))
+                response = input('Let\'s try typing the request instead -->\n')
 
-		return response
+        else:
+            response = input('please type your response\n')
 
-	# Converts text to audio. Uses Bing Speech API
-	def text_to_speech(self, text, file_name='sounds/output.mp3', play_audio=True):
+        return response
 
-		# retrieve jwt
-		url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
-		headers = {
-			'Ocp-Apim-Subscription-Key': self.speech_key
-		}
-		bing_jwt = requests.post(url, headers=headers).text
+    # Converts text to audio. Uses Bing Speech API
+    def text_to_speech(self, text, file_name='assets/output.mp3', play_audio=True):
+
+        # retrieve jwt
+        url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.speech_key
+        }
+        bing_jwt = requests.post(url, headers=headers).text
 
 
-		url = 'https://speech.platform.bing.com/synthesize'
-		headers = {
-			'Content-Type':	'application/ssml+xml',
-			'X-Microsoft-OutputFormat': 'audio-16khz-128kbitrate-mono-mp3',
-			'User-Agent': 'gLabs Speech',
-			'Authorization': 'Bearer ' + bing_jwt 
-		}
+        url = 'https://speech.platform.bing.com/synthesize'
+        headers = {
+            'Content-Type':	'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'audio-16khz-128kbitrate-mono-mp3',
+            'User-Agent': 'gLabs Speech',
+            'Authorization': 'Bearer ' + bing_jwt 
+        }
 
-		payload = """
-			<speak version='1.0' xml:lang='en-US'>
-				<voice xml:lang='en-US' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>
-				{}
-				</voice>
-			</speak>
-				  """.format(text)
-		sound_file = requests.post(url, headers=headers, data=payload).content
+        payload = """
+            <speak version='1.0' xml:lang='en-US'>
+                <voice xml:lang='en-US' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>
+                {}
+                </voice>
+            </speak>
+                """.format(text)
+        sound_file = requests.post(url, headers=headers, data=payload).content
 
-		with open(file_name, 'wb') as f:
-			f.write(sound_file)
+        with open(file_name, 'wb') as f:
+            f.write(sound_file)
 
-		if play_audio:
-			mixer.init(14250, -16, 2, 4096)
-			mixer.music.load(file_name)
-			mixer.music.play(loops=1, start=0.0)
-			while mixer.music.get_busy() == True:
-				continue
-	
-	# Sets value in sheet_range in gSheet 
-	def set_uw_values(self, sheet_range, value):
+        if play_audio:
+            mixer.init(14250, -16, 2, 4096)
+            mixer.music.load(file_name)
+            mixer.music.play(loops=1, start=0.0)
+            while mixer.music.get_busy() == True:
+                continue
+    
+    # Sets value in sheet_range in gSheet 
+    def set_uw_values(self, sheet_range, value):
 
-		return self.gSheet.set_value(sheet_range, value)
+        return self.gSheet.set_value(sheet_range, value)
 
-	# Pulls FHA quote from Quote Results in gSheet
-	def get_uw_values(self):
+    # Pulls FHA quote from Quote Results in gSheet
+    def get_uw_values(self):
 
-		loans = self.gSheet.get_values('Quote Results!A2:AM')
-		fha_loan = loans[-1]
+        loans = self.gSheet.get_values('Quote Results!A2:AM')
+        fha_loan = loans[-1]
 
-		return fha_loan
+        return fha_loan
 
-	# Sends string to bing for address standardization. Uses Bing Maps API
-	def get_bing_address(self, address):
+    # Sends string to bing for address standardization. Uses Bing Maps API
+    def get_bing_address(self, address):
 
-		url = 'https://atlas.microsoft.com/search/address/json'
-		params = {
-			'subscription-key': self.map_key, 
-			'api-version': '1.0',
-			'query': address
-		}
-		res = requests.get(url, params=params).json()
+        url = 'https://atlas.microsoft.com/search/address/json'
+        params = {
+            'subscription-key': self.map_key, 
+            'api-version': '1.0',
+            'query': address
+        }
+        res = requests.get(url, params=params).json()
 
-		bing_address = {
-			'street_numb' : res['results'][0]['address']['streetNumber'],
-			'street_name' : res['results'][0]['address']['streetName'],
-			'city'        : res['results'][0]['address']['municipality'].split(',')[0],
-			'state'       : res['results'][0]['address']['countrySubdivision'],
-			'zip'         : res['results'][0]['address']['postalCode'],
-			'county'      : res['results'][0]['address']['countrySecondarySubdivision']
-		}
+        bing_address = {
+            'street_numb' : res['results'][0]['address']['streetNumber'],
+            'street_name' : res['results'][0]['address']['streetName'],
+            'city'        : res['results'][0]['address']['municipality'].split(',')[0],
+            'state'       : res['results'][0]['address']['countrySubdivision'],
+            'zip'         : res['results'][0]['address']['postalCode'],
+            'county'      : res['results'][0]['address']['countrySecondarySubdivision']
+        }
 
-		return bing_address
+        return bing_address
 
-	# Plays chime to prompt user to speak
-	def play_prompt(self, file='sounds/blip.mp3'):
-		
-		mixer.init(20500, -16, 1, 4096)
-		mixer.music.load(file)
-		mixer.music.play(loops=0, start=0.0)
-		while mixer.music.get_busy() == True:
-			continue
+    # Plays chime to prompt user to speak
+    def play_prompt(self, file='assets/blip.mp3'):
 
-	# Gets a random thought from Ryan Swanson
-	def get_thought(self):
+        mixer.init(20500, -16, 1, 4096)
+        mixer.music.load(file)
+        mixer.music.play(loops=0, start=0.0)
+        while mixer.music.get_busy() == True:
+            continue
 
-		joke = requests.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes').text
-		self.text_to_speech(joke)
-		print(joke)
+    # Gets a random thought from Ryan Swanson
+    def get_thought(self):
+
+        joke = requests.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes').text
+        self.text_to_speech(joke)
+        print(joke)
 
 
 if __name__ == '__main__':
-	BING_MAPKEY    = 'eFOm0_-7ZzYVCsi7Dbku3aF85gjHZwVuhGFcBOv2GJ0'
-	BING_SPEECHKEY = '8c51ff7c89c9438fa9e720ffa9530db3'
-	LUIS_APPID     = 'cab6d4fe-10bc-4ed1-85f6-eccc6b385ad0'
-	LUIS_APPKEY    = '8092489ce228440bba290f5e6425204b'
-	gSheet_SSKEY   = '1Y9VYZbQeUaRWYxCZlmtpuhcwpZqkbfn6wrbRq_UFNH4'
 
-	vs = Voice_Sizer(BING_MAPKEY, BING_SPEECHKEY, LUIS_APPID, LUIS_APPKEY, gSheet_SSKEY)
+    load_dotenv()
+
+    keys = [
+        os.getenv('BING_MAPKEY'),
+        os.getenv('BING_SPEECHKEY'),
+        os.getenv('LUIS_APPID'),
+        os.getenv('LUIS_APPKEY'),
+        os.getenv('GSHEET_SSKEY')
+    ]
+
+    if None in keys:
+        print("Key Error: Please ensure the following env vars are set: 'BING_MAPKEY', 'BING_SPEECHKEY', 'LUIS_APPID', 'LUIS_APPKEY', 'GSHEET_SSKEY'")
+    else:
+        Voice_Sizer(BING_MAPKEY, BING_SPEECHKEY, LUIS_APPID, LUIS_APPKEY, gSheet_SSKEY)
--- readme.md
+++ readme.md
@@ -1,25 +1,85 @@
-## Voice Sizer
+# Voice Sizer
 
-Simple sizing via vocal prompts. Can be used with user audio mic or typed responses.
+A fun project where a user can get live HUD loan terms on a property with voice commands (or typed responses - works as a chatbot too). 
 
-Uses Bing Speech API for vocals, Bing Maps for Address, and Microsoft's LUIS.ai for language understanding.
+Uses Bing Speech API for vocals, Bing Maps for Address, and Microsoft's LUIS.ai for language understanding. Connects to Google Sheets for UW.
 
-Connects to Google Sheets for UW.
 
-Uses really crappy pygame mixer for audio - apologies for the weird, usually not working prompt sound!
+![Flow diagram](./assets/diagram.png)
 
-### Install
+1. User vocalizes responses
+2. Microsoft Cognitive Speech Recognition converts speech to text
+3. LUIS AI pulls out key entities from speech text
+4. Voice Sizer takes speech entities and routes to appropriate action
+*(if new sizing)*
+5. Voice Sizer sends spoken address to Bing Maps for address standardization (required for proper underwriting).
+6. Voice Sizer combines standard address with user spoken values to send to Google Sheet Underwriting Model
+7. Underwriting Model responds with loan terms. Voice Sizer speaks loan term results.
+8. Profit!
+
+## Install
 
 `pip install requirements.txt`
 
-`python create_sizing.py`
+PyAudio depends on another package, `portaudio`. If PyAudio fails to install, install `portaudio` with `brew install portaudio` first. Then rerun `pip installl pyaudio`.
 
-API keys are stored at the bottom of `create_sizing.py`
+You'll need the following env vars (which can be set in a .env file or set explicitly):
+
+| Env Var        | Description |
+| -------------- | ------------- |
+| BING_MAPKEY    | Bing Maps used for address recognition. [Instructions on how to get a Bing Map Key](https://docs.microsoft.com/en-us/bingmaps/getting-started/bing-maps-dev-center-help/getting-a-bing-maps-key)  |
+| BING_SPEECHKEY | Bing speech used for speech to text recognition. [Instructions on how to get a Bing Speech Key](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/overview#try-the-speech-service-for-free)  |
+| LUIS_APPID     | Microsoft LUIS is used for text entity recognition. For example, it can parse that you want to "create a sizing" from the following requests: "I would like to create a sizing" or "please create a sizing for me". [Instructions on how to get a LUIS App ID & App Key](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/quickstarts/create-luis)  |
+| LUIS_APPKEY    | See above  |
+| GSHEET_SSKEY   | Google Sheets are used to run the property underwriting. The Sheet ID for the UW model is "1Y9VYZbQeUaRWYxCZlmtpuhcwpZqkbfn6wrbRq_UFNH4". Email for permissions to access! |
+
+
+## Usage
+
+Start the show with:
+
+`python create_sizing.py`
 
 First time run, google will open a browser window for OAuth to the google sheet
 
-### Not quite completed
 
-- Address saves to a single cell in google sheet instead of being split
-- Only FHA quote is returned for now
+The following is an example sizing:
 
+---
+
+**Voice Sizer: Hello, how can I help you?**
+
+*Hi, I would like to create a new property sizing.*
+
+**VS: Got it, you would like to create a sizing.**
+
+**VS: What is the address of the property?**
+
+*152 West 57th street, New York, New York*
+
+**VS: How much did the property collect in rent last year?**
+
+*Three million five hundred twenty thousand dollars*
+
+**VS: What were the total expenses for the property last year?**
+
+*About one million eight hundred thousand*
+
+**VS: How many units are available at the property?**
+
+*There are two hundred and seventy five units available*
+
+**VS: Thank you, sizing property now.**
+
+
+`... Sizing property`
+
+**VS: Finished with sizing, pulling FHA loan terms...**
+
+**VS: We have a loan amount of thirty million five hundred and seventy thousand at a three point thirty rate for a thirty five year term.**
+
+**VS: This sizing was completed in two point sixty-three seconds. Beat that Ryan Patterson!**
+
+---
+
+Note, Ryan Patterson was only slightly hurt by the presentation. He is still considered the rising star underwriter and is honored to be the benchmark.
